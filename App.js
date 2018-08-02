b83af23a71013ab01a0e13f936a09307283cbc94
Hiroshi Imamasu <hiroshimashu@gmail.com>
2018-08-02 16:31:33 +0900

    add postMessage and onMessage

--- App.js
+++ App.js
@@ -2,14 +2,30 @@ import React, { Component } from 'react';
 import { WebView } from 'react-native';
 
 class MyWeb extends Component {
+  onMessage = (event) => {
+     const { data } = event.nativeEvent;
+     if (data === 'clicked') {
+        console.log('js is working')
+     } else if (data === 'NG') {
+       alert('NG');
+     } else {
+       console.log('js is working');
+     } 
+  }
+
   render() {
     return (
       <WebView
-        source={{uri: 'https://www.google.co.jp/imghp?hl=ja'}}
+        source={{uri:'https://www.google.co.jp/imghp?hl=ja'}}
         style={{marginTop: 20}}
+        injectedJavaScript = {js}
       />
     );
   }
 }
+
+const js = `
+    window.postMessage('clicked');
+`
 
 export default MyWeb;
